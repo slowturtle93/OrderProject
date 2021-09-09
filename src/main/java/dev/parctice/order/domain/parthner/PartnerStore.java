db43ebb8c51c231fec0605048143a1eae86a916e
gkrthdz <gkrthdz@naver.com>
2021-09-09 10:43:18 +0900

    # 오타 수정

--- src/main/java/dev/parctice/order/domain/parthner/PartnerStore.java
+++ src/main/java/dev/parctice/order/domain/parthner/PartnerStore.java
@@ -1,7 +1,7 @@
 package dev.parctice.order.domain.parthner;
 
 /**
- * 파트너 도메인 저자을 담당하는 클래스 파일
+ * 파트너 도메인 저장을 담당하는 클래스 파일
  */
 public interface PartnerStore {
     Partner store(Partner initPartner);
