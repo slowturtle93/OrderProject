a8deeff20ed9a0837d40559307710d4c6def4481
gkrthdz <gkrthdz@naver.com>
2021-10-06 10:37:16 +0900

    feat: mail sender 구현

--- src/main/java/dev/parctice/order/infrastructure/NotificationExecutor.java
+++ src/main/java/dev/parctice/order/infrastructure/NotificationExecutor.java
@@ -2,6 +2,10 @@ package dev.parctice.order.infrastructure;
 
 import dev.parctice.order.domain.notification.NotificationService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,9 +14,24 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class NotificationExecutor implements NotificationService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String FROM_ADDRESS;
+
     @Override
     public void sendEmail(String email, String title, String description) {
         log.info("sendEmail");
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setFrom(FROM_ADDRESS);
+        message.setSubject(title);
+        message.setText(description);
+
+        mailSender.send(message);
     }
 
     @Override
